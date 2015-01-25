c2ab6cddb8a3eb5fe5314220faddffbfa5c73e21
ametico <ametico@gmail.com>
2015-01-24 22:56:43 -0600

    Modificación del script

--- Scripts Base Datos/Script Funciones Fechas de Usuarios y Roles.sql
+++ Scripts Base Datos/Script Funciones Fechas de Usuarios y Roles.sql
@@ -3,11 +3,11 @@ CREATE FUNCTION seguridad.f_FechasUsuario() RETURNS VOID
 AS
 $func$
 UPDATE seguridad.usuarios 
-	set estado = TRUE
-	where fecha_activacion = current_date;
-UPDATE seguridad.usuarios 
        set estado = FALSE
        where fecha_desactivacion = current_date;
+UPDATE seguridad.usuarios 
+	set estado = TRUE
+	where fecha_activacion = current_date;
 $func$
 LANGUAGE SQL;
 
