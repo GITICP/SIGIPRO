2dbfbbb9d08b5620f32cfee99405f8363cc1825f
jchaconbogarin <jdcb92@gmail.com>
2015-03-08 14:06:19 -0600

    Se agregó tabla de permisos para ver subbodega y se eliminaron los serial de las otras tablas de permisos.

---
+++ Scripts Base Datos/Modificaciones/Tabla de permisos para ver sub bodegas.sql
@@ -0,0 +1,31 @@
+DROP TABLE bodega.usuarios_sub_bodegas_ingresos;
+DROP TABLE bodega.usuarios_sub_bodegas_egresos;
+
+CREATE TABLE bodega.usuarios_sub_bodegas_ingresos ( 
+	id_sub_bodega integer NOT NULL,
+        id_usuario integer NOT NULL 
+ ); 
+
+CREATE TABLE bodega.usuarios_sub_bodegas_egresos ( 
+	id_sub_bodega integer NOT NULL,
+        id_usuario integer NOT NULL 
+ ); 
+
+CREATE TABLE bodega.usuarios_sub_bodegas_ver (
+        id_sub_bodega integer NOT NULL,
+        id_usuario integer NOT NULL
+);
+
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ingresos ADD CONSTRAINT pk_usuarios_sub_bodegas_ingresos PRIMARY KEY (id_sub_bodega, id_usuario);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_egresos ADD CONSTRAINT pk_usuarios_sub_bodegas_egresos PRIMARY KEY (id_sub_bodega, id_usuario);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ver ADD CONSTRAINT pk_usuarios_sub_bodegas_ver PRIMARY KEY (id_sub_bodega, id_usuario);
+
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ingresos ADD CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES seguridad.usuarios(id_usuario);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_egresos ADD CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES seguridad.usuarios(id_usuario);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ver ADD CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES seguridad.usuarios(id_usuario);
+
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ingresos ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_egresos ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
+ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ver ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
+
+
