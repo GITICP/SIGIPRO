c14cbdd847eb174fc2b1da41c6f12d99ada6138c
jchaconbogarin <jdcb92@gmail.com>
2015-03-09 22:56:10 -0600

    Agregado campo fecha_vencimiento de sub bodega

--- Scripts Base Datos/Modificaciones/Tabla de permisos para ver sub bodegas.sql
+++ Scripts Base Datos/Modificaciones/Tabla de permisos para ver sub bodegas.sql
@@ -1,5 +1,6 @@
 DROP TABLE bodega.usuarios_sub_bodegas_ingresos;
 DROP TABLE bodega.usuarios_sub_bodegas_egresos;
+DROP TABLE bodega.inventarios_sub_bodegas;
 
 CREATE TABLE bodega.usuarios_sub_bodegas_ingresos ( 
 	id_sub_bodega integer NOT NULL,
@@ -16,6 +17,13 @@ CREATE TABLE bodega.usuarios_sub_bodegas_ver (
         id_usuario integer NOT NULL
 );
 
+CREATE TABLE bodega.inventarios_sub_bodegas ( 
+	id_sub_bodega integer,
+	id_producto integer,
+     	cantidad integer NOT NULL,
+        fecha_vencimiento date NULL
+ );
+
 ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ingresos ADD CONSTRAINT pk_usuarios_sub_bodegas_ingresos PRIMARY KEY (id_sub_bodega, id_usuario);
 ALTER TABLE ONLY bodega.usuarios_sub_bodegas_egresos ADD CONSTRAINT pk_usuarios_sub_bodegas_egresos PRIMARY KEY (id_sub_bodega, id_usuario);
 ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ver ADD CONSTRAINT pk_usuarios_sub_bodegas_ver PRIMARY KEY (id_sub_bodega, id_usuario);
@@ -28,4 +36,5 @@ ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ingresos ADD CONSTRAINT fk_id_sub_b
 ALTER TABLE ONLY bodega.usuarios_sub_bodegas_egresos ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
 ALTER TABLE ONLY bodega.usuarios_sub_bodegas_ver ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
 
-
+ALTER TABLE ONLY bodega.inventarios_sub_bodegas ADD CONSTRAINT pk_inventarios_sub_bodegas_permite_fecha_null UNIQUE (id_sub_bodega, id_producto, fecha_vencimiento);
+ALTER TABLE ONLY bodega.inventarios_sub_bodegas ADD CONSTRAINT fk_id_sub_bodega FOREIGN KEY (id_sub_bodega) REFERENCES bodega.sub_bodegas(id_sub_bodega);
