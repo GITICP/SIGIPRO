02ca3aef691f52fabed9e1565955ab4fc9355d90
Walter <Walter@192.168.0.4>
2015-03-24 00:17:21 -0600

    Insertar en dao

--- src/java/com/icp/sigipro/caballeriza/dao/CaballoDAO.java
+++ src/java/com/icp/sigipro/caballeriza/dao/CaballoDAO.java
@@ -5,10 +5,66 @@
  */
 package com.icp.sigipro.caballeriza.dao;
 
+import com.icp.sigipro.basededatos.SingletonBD;
+import com.icp.sigipro.caballeriza.modelos.Caballo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Walter
  */
 public class CaballoDAO {
+    private Connection conexion;
     
+    
+    public CaballoDAO()
+    {
+        SingletonBD s = SingletonBD.getSingletonBD();
+        conexion = s.conectar();
+    } 
+    
+    public boolean insertarCaballo(Caballo c){
+        boolean resultado = false;
+        try{
+            PreparedStatement consulta = getConexion().prepareStatement(" INSERT INTO caballeriza.caballos (nombre, numero_microchip,fecha_nacimiento,fecha_ingreso,sexo,color,otras_sennas,estado,id_grupo_de_caballo) " +
+                                                             " VALUES (?,?,?,?,?,?,?,?,?) RETURNING id_caballo");
+            consulta.setString(1, c.getNombre());
+            consulta.setInt(2, c.getNumero_microchip());
+            consulta.setDate(3, c.getFecha_nacimiento());
+            consulta.setDate(4,c.getFecha_ingreso());
+            consulta.setString(5,c.getSexo());
+            consulta.setString(6,c.getColor());
+            consulta.setString(7, c.getOtras_sennas());
+            consulta.setBoolean(8, c.isEstado());
+            consulta.setInt(9, c.getGrupo_de_caballos().getId_grupo_caballo());
+            
+            ResultSet resultadoConsulta = consulta.executeQuery();
+            if ( resultadoConsulta.next() ){
+                resultado = true;
+                c.setId_caballo(resultadoConsulta.getInt("id_caballo"));
+            }
+            consulta.close();
+            conexion.close();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return resultado;
+    }
+    
+    private Connection getConexion(){
+        try{     
+            if ( conexion.isClosed() ){
+                SingletonBD s = SingletonBD.getSingletonBD();
+                conexion = s.conectar();
+            }
+        }
+        catch(Exception ex)
+        {
+            conexion = null;
+        }
+        return conexion;
+    }     
 }
