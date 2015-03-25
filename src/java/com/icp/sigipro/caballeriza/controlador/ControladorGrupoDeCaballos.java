cc71ba9f53cc01209fe555b447eaec5d71be9b17
Walter <Walter@WCU>
2015-03-25 08:54:30 -0600

    ......

--- src/java/com/icp/sigipro/caballeriza/controlador/ControladorGrupoDeCaballos.java
+++ src/java/com/icp/sigipro/caballeriza/controlador/ControladorGrupoDeCaballos.java
@@ -124,7 +124,7 @@ public class ControladorGrupoDeCaballos extends SIGIPROServlet {
   }
     @Override
     protected int getPermiso() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
