0c7f5d029cad3d4c49d9b6545a72e8a1171b2f37
jchaconbogarin <jdcb92@gmail.com>
2015-03-27 01:00:36 -0600

    Mejora en el rendimiento de inventarios, solicitudes y préstamos

--- src/java/com/icp/sigipro/bodegas/dao/InventarioDAO.java
+++ src/java/com/icp/sigipro/bodegas/dao/InventarioDAO.java
@@ -5,11 +5,11 @@
  */
 package com.icp.sigipro.bodegas.dao;
 
-import com.icp.sigipro.basededatos.SingletonBD;
 import com.icp.sigipro.bodegas.modelos.Inventario;
+import com.icp.sigipro.bodegas.modelos.ProductoInterno;
 import com.icp.sigipro.configuracion.dao.SeccionDAO;
+import com.icp.sigipro.configuracion.modelos.Seccion;
 import com.icp.sigipro.core.DAO;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -59,10 +59,18 @@ public class InventarioDAO extends DAO<Inventario> {
     try {
       PreparedStatement consulta;
       if (flag_prestamos==0)
-        { consulta = getConexion().prepareStatement(" SELECT * FROM bodega.inventarios Where id_seccion != ? AND id_seccion != 0");
+        { consulta = getConexion().prepareStatement(" SELECT i.stock_actual as stock_actual, ci.codigo_icp as codigo_icp, ci.presentacion as presentacion, i.id_inventario as id_inventario, i.id_producto as id_producto, i.id_seccion as id_seccion, ci.nombre as nombre_producto, s.nombre_seccion as nombre_seccion "
+                                                  + " FROM bodega.inventarios i "
+                                                  + "           INNER JOIN bodega.catalogo_interno ci on i.id_producto = ci.id_producto "
+                                                  + "           INNER JOIN seguridad.secciones s on i.id_seccion = s.id_seccion "
+                                                  + " Where i.id_seccion != ? AND i.id_seccion != 0 ");
           }
       else
-        {consulta = getConexion().prepareStatement(" SELECT * FROM bodega.inventarios Where id_seccion = ? or id_seccion = 0 ");
+        {consulta = getConexion().prepareStatement(" SELECT i.stock_actual as stock_actual, ci.codigo_icp as codigo_icp, ci.presentacion as presentacion, i.id_inventario as id_inventario, i.id_producto as id_producto, i.id_seccion as id_seccion, ci.nombre as nombre_producto, s.nombre_seccion as nombre_seccion "
+                                                  + " FROM bodega.inventarios i "
+                                                  + "           INNER JOIN bodega.catalogo_interno ci on i.id_producto = ci.id_producto "
+                                                  + "           INNER JOIN seguridad.secciones s on i.id_seccion = s.id_seccion "
+                                                  + " Where i.id_seccion = ? or i.id_seccion = 0;");
          }
       consulta.setInt(1, id_seccion);
       ResultSet rs = consulta.executeQuery();
@@ -73,14 +81,16 @@ public class InventarioDAO extends DAO<Inventario> {
         inventario.setId_producto(rs.getInt("id_producto"));
         inventario.setId_seccion(rs.getInt("id_seccion"));
         inventario.setStock_actual(rs.getInt("stock_actual"));
-        try {
-          ProductoInternoDAO pr = new ProductoInternoDAO();
-          SeccionDAO sc = new SeccionDAO();
-          inventario.setProducto(pr.obtenerProductoInterno(rs.getInt("id_producto")));
-          inventario.setSeccion(sc.obtenerSeccion(rs.getInt("id_seccion")));
-        } catch (Exception ex) {
-          ex.printStackTrace();
-        }
+        ProductoInterno p = new ProductoInterno();
+        p.setId_producto(rs.getInt("id_producto"));
+        p.setNombre(rs.getString("nombre_producto"));
+        p.setPresentacion(rs.getString("presentacion"));
+        p.setCodigo_icp(rs.getString("codigo_icp"));
+        Seccion s = new Seccion();
+        s.setId_seccion(rs.getInt("id_seccion"));
+        s.setNombre_seccion(rs.getString("nombre_seccion"));
+        inventario.setProducto(p);
+        inventario.setSeccion(s);
         resultado.add(inventario);
       }
       rs.close();
@@ -97,25 +107,28 @@ public class InventarioDAO extends DAO<Inventario> {
     List<Inventario> resultado = new ArrayList<Inventario>();
 
     try {
-        PreparedStatement consulta = getConexion().prepareStatement("SELECT * FROM bodega.inventarios");
+        PreparedStatement consulta = getConexion().prepareStatement(" SELECT i.stock_actual as stock_actual, i.id_inventario as id_inventario, i.id_producto as id_producto, i.id_seccion as id_seccion, ci.nombre as nombre_producto, s.nombre_seccion as nombre_seccion "
+                                                                  + " FROM bodega.inventarios i "
+                                                                  + "           INNER JOIN bodega.catalogo_interno ci on i.id_producto = ci.id_producto "
+                                                                  + "           INNER JOIN seguridad.secciones s on i.id_seccion = s.id_seccion;");
 
         ResultSet rs = consulta.executeQuery();
 
         while (rs.next()) {
             Inventario inventario = new Inventario();
-            System.out.println(rs.getInt("id_inventario"));
             inventario.setId_inventario(rs.getInt("id_inventario"));
             inventario.setId_producto(rs.getInt("id_producto"));
             inventario.setId_seccion(rs.getInt("id_seccion"));
             inventario.setStock_actual(rs.getInt("stock_actual"));
-            try {
-                ProductoInternoDAO pr = new ProductoInternoDAO();
-                SeccionDAO sc = new SeccionDAO();
-                inventario.setProducto(pr.obtenerProductoInterno(rs.getInt("id_producto")));
-                inventario.setSeccion(sc.obtenerSeccion(rs.getInt("id_seccion")));
-            }catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            ProductoInterno p = new ProductoInterno();
+            p.setId_producto(rs.getInt("id_producto"));
+            p.setNombre(rs.getString("nombre_producto"));
+            Seccion s = new Seccion();
+            s.setId_seccion(rs.getInt("id_seccion"));
+            s.setNombre_seccion(rs.getString("nombre_seccion"));
+            inventario.setProducto(p);
+            inventario.setSeccion(s);
+            
             resultado.add(inventario);
         }
         rs.close();
