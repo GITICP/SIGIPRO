41383067da5331618d56f544537ec202597ab856
jchaconbogarin <jdcb92@gmail.com>
2015-04-30 22:39:22 -0600

    Merge completo

--- Scripts Base Datos/Script Creación Completa.bat
+++ Scripts Base Datos/Script Creación Completa.bat
@@ -3,5 +3,5 @@ chcp 1252
 set PGPASSWORD=Solaris2014
 set PGUSER=postgres
 @echo on
-for %%G in (Scripts/*.sql) do "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -d merge_menu -f "Scripts/%%G"
+for %%G in (Scripts/*.sql) do "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -d sigipro -f "Scripts/%%G"
 pause
