6e6cee7f28334a329a992bb1f861677814b870e1
jchaconbogarin <jdcb92@gmail.com>
2015-05-04 12:32:45 -0600

    Se eliminó sangría de prueba del menú

--- Scripts Base Datos/Scripts/2015-04-06 - 2 - Script Caballeriza.sql
+++ Scripts Base Datos/Scripts/2015-04-06 - 2 - Script Caballeriza.sql
@@ -190,7 +190,7 @@ INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, 
 INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (404, 400, 'Eventos Clínicos', '/Caballeriza/EventoClinico');
 --INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (405, 400, 'Inóculos', '/Caballeriza/Inoculo');
 --INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (407, 400, 'Sangría', '/Caballeriza/Sangria');
-INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (406, 400, 'Pruebas Sangría', '/Caballeriza/SangriaPrueba');
+--INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (406, 400, 'Pruebas Sangría', '/Caballeriza/SangriaPrueba');
 
 --Permisos Menu Principal
 
