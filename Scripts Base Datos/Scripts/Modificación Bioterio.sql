c8809cfd4029a6a83dd20e2e7e63d6fbf351e3fd
ametico <ametico@gmail.com>
2015-05-06 16:38:40 -0600

    Modificaciones adicionales de Bioterio

--- Scripts Base Datos/Scripts/Modificación Bioterio.sql
+++ Scripts Base Datos/Scripts/Modificación Bioterio.sql
@@ -6,3 +6,5 @@ ADD COLUMN detalle_procedencia character varying(250);
 
 CREATE UNIQUE INDEX i_identificador ON bioterio.gruposhembras USING btree (identificador);
 
+ALTER TABLE bioterio.cruces
+ADD COLUMN fecha_estimada_parto date;
