a63717efd66479fda19ad4d8f1ca1b66511c7009
jchaconbogarin <jdcb92@gmail.com>
2015-06-07 23:20:24 -0600

    Arreglo en tilde del menú

--- Scripts Base Datos/Scripts/2015-06-08 - 09 - Modificaciones Conejera.sql
+++ Scripts Base Datos/Scripts/2015-06-08 - 09 - Modificaciones Conejera.sql
@@ -1,7 +1,7 @@
 ALTER TABLE ONLY bioterio.conejos_produccion ADD CONSTRAINT pk_conejos_produccion PRIMARY KEY (id_produccion);
 
-INSERT INTO seguridad.permisos(id_permiso, nombre, descripcion) VALUES (256, '[Bioterio]AdministrarConejosProduccion', 'Permite agregar/editar/eliminar grupos de conejos de produccion');
-INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (256, 250, 'Conejos Produccion', '/Conejera/ConejosProduccion');
+INSERT INTO seguridad.permisos(id_permiso, nombre, descripcion) VALUES (256, '[Bioterio]AdministrarConejosProducción', 'Permite agregar/editar/eliminar grupos de conejos de producción');
+INSERT INTO seguridad.entradas_menu_principal(id_menu_principal, id_padre, tag, redirect) VALUES (256, 250, 'Conejos Producción', '/Conejera/ConejosProduccion');
 INSERT INTO seguridad.permisos_menu_principal(id_permiso, id_menu_principal) VALUES (256, 256);
 
 ALTER TABLE bioterio.cepas  ADD  descripcion character varying(200);
