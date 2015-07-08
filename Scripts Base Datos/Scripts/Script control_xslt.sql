198e77ddcb875a99bb84c43bc1ae51d040351c27
jchaconbogarin <jdcb92@gmail.com>
2015-07-07 18:57:52 -0600

    Arreglo de script de control de xslt

--- Scripts Base Datos/Scripts/Script control_xslt.sql
+++ Scripts Base Datos/Scripts/Script control_xslt.sql
@@ -5,5 +5,5 @@ CREATE TABLE control_xslt.control_xslt (
     id_control_xslt serial NOT NULL,
     nombre varchar(50) NOT NULL,
     estructura XML NOT NULL,
-    CONSTRAINT control_xslt PRIMARY KEY (id_control_xslt)
+    CONSTRAINT control_xslt_pk PRIMARY KEY (id_control_xslt)
 );
