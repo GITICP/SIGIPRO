55002945ea16a28a9ffaa412a6fc3cae01fb69a6
Boga <Boga@DESKTOP-9FGJ608>
2015-08-24 21:23:09 -0600

    Arreglo ortográfico

--- web/recursos/js/sigipro/Equipo.js
+++ web/recursos/js/sigipro/Equipo.js
@@ -17,10 +17,10 @@ $(document).ready(function(){
         var label_certificado = $("#label-certificado");
         
         if (certificable === true) {
-            label_certificado.text("*Certficado");
+            label_certificado.text("*Certificado");
             certificado.prop("required", true);
         } else {
-            label_certificado.text("Certficado");
+            label_certificado.text("Certificado");
             certificado.prop("required", false);
         }
     });
