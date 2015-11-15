7c21910599c672e6fc5360a257939698a6ff6f5e
Josue <Josue@JosuePC>
2015-11-15 11:36:06 -0600

    Modelos completos de inoculo y veneno_produccion

--- src/java/com/icp/sigipro/produccion/modelos/Veneno_Producción.java
+++ src/java/com/icp/sigipro/produccion/modelos/Veneno_Producción.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.icp.sigipro.produccion.modelos;
+import java.lang.reflect.Field;
 import org.json.JSONObject;
 import java.sql.Date;
 import java.text.DateFormat;
@@ -19,4 +20,79 @@ public class Veneno_Producción {
   private int cantidad;
   private Date fecha_ingreso;
   private String observaciones;
+
+    public String parseJSON(){
+        Class _class = this.getClass();
+        JSONObject JSON = new JSONObject();
+        try{
+            Field properties[] = _class.getDeclaredFields();
+            for (int i = 0; i < properties.length; i++) {
+                Field field = properties[i];
+                if (i != 0){
+                    JSON.put(field.getName(), field.get(this));
+                }else{
+                    JSON.put("id_objeto", field.get(this));
+                }
+            }
+        }catch (Exception e){
+            
+        }
+        return JSON.toString();
+    }
+
+  
+    public int getId_veneno() {
+        return id_veneno;
+    }
+
+    public void setId_veneno(int id_veneno) {
+        this.id_veneno = id_veneno;
+    }
+
+    public String getVeneno() {
+        return veneno;
+    }
+
+    public void setVeneno(String veneno) {
+        this.veneno = veneno;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFecha_ingreso() {
+        return fecha_ingreso;
+    }
+
+    private String formatearFecha(Date fecha)
+    {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        return df.format(fecha);
+    }  
+    
+    public String getFecha_ingreso_S(){
+        if (this.fecha_ingreso != null)
+            {return formatearFecha(this.fecha_ingreso);}
+        else
+            {return "";}
+    }
+    
+    public void setFecha_ingreso(Date fecha_ingreso) {
+        this.fecha_ingreso = fecha_ingreso;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+  
+  
 }
