3f6296d9da59a4be6bdf45e29a7f01074feb515e
Amed <Amed@AmedPC>
2015-11-15 10:44:26 -0600

    Update de unas tablas y modelos

--- src/java/com/icp/sigipro/produccion/modelos/Formula_Maestra.java
+++ src/java/com/icp/sigipro/produccion/modelos/Formula_Maestra.java
@@ -13,4 +13,29 @@ public class Formula_Maestra {
   private int id_formula_m;
   private String nombre;
   private String descripcion;
+
+  public int getId_formula_m() {
+    return id_formula_m;
+  }
+
+  public void setId_formula_m(int id_formula_m) {
+    this.id_formula_m = id_formula_m;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+  public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
+  
 }
