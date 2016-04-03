11ad77fdedb661f0f56d9cd147918fe3f9b3c679
Jose Chacón Bogarín <jdcb92@gmail.com>
2016-04-03 13:33:17 -0600

    Commit para actualizar SingletonBD

--- src/java/com/icp/sigipro/basededatos/SingletonBD.java
+++ src/java/com/icp/sigipro/basededatos/SingletonBD.java
@@ -29,7 +29,7 @@ public class SingletonBD
 
             // Configuración de Base de Datos
             cpds.setDriverClass("org.postgresql.Driver");
-            cpds.setJdbcUrl("jdbc:postgresql://localhost/sigipro");
+            cpds.setJdbcUrl("jdbc:postgresql://localhost/sigipro_cc");
             cpds.setUser("postgres");
             cpds.setPassword("Solaris2014");
 
