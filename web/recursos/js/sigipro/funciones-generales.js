325b96be0492f9c446a18f920cd7679d98b8318b
jchaconbogarin <jchaconbogarin@gmail.com>
2016-06-29 12:55:34 -0600

    Arreglo de bug con salto de línea en textarea

--- web/recursos/js/sigipro/funciones-generales.js
+++ web/recursos/js/sigipro/funciones-generales.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).keydown(function (event) {
-        if (event.keyCode === 13 && event.target.localName !== "button") {
+        if (event.keyCode === 13 && event.target.localName !== "button" && event.target.localName !== "textarea") {
             event.preventDefault();
             return false;
         } else {
