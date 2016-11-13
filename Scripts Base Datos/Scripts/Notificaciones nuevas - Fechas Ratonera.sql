5ef385e5abb73c2c59aec58dbeed6429f4c4f51a
Amed <Amed@AmedPC>
2016-11-13 17:06:00 -0600

    Minor bug fix - eliminar datos de debug

--- Scripts Base Datos/Scripts/Notificaciones nuevas - Fechas Ratonera.sql
+++ Scripts Base Datos/Scripts/Notificaciones nuevas - Fechas Ratonera.sql
@@ -2,22 +2,22 @@
 --Ratonera
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (103, 'Retiro de pie de cría próximo', 'sigipro-mouse-1', ARRAY[209,1]::INT[], ARRAY[]::INT[]);
+    VALUES (103, 'Retiro de pie de cría próximo', 'sigipro-mouse-1', ARRAY[209]::INT[], ARRAY[]::INT[]);
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (104, 'Apareamiento de cara próximo', 'sigipro-mouse-1', ARRAY[201,1]::INT[], ARRAY[]::INT[]);
+    VALUES (104, 'Apareamiento de cara próximo', 'sigipro-mouse-1', ARRAY[201]::INT[], ARRAY[]::INT[]);
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (105, 'Eliminación de macho próxima', 'sigipro-mouse-1', ARRAY[201,1]::INT[], ARRAY[]::INT[]);
+    VALUES (105, 'Eliminación de macho próxima', 'sigipro-mouse-1', ARRAY[201]::INT[], ARRAY[]::INT[]);
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (106, 'Eliminación de hembra próxima', 'sigipro-mouse-1', ARRAY[201,1]::INT[], ARRAY[]::INT[]);
+    VALUES (106, 'Eliminación de hembra próxima', 'sigipro-mouse-1', ARRAY[201]::INT[], ARRAY[]::INT[]);
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (107, 'Selección para cara próxima', 'sigipro-mouse-1', ARRAY[201,1]::INT[], ARRAY[]::INT[]);
+    VALUES (107, 'Selección para cara próxima', 'sigipro-mouse-1', ARRAY[201]::INT[], ARRAY[]::INT[]);
 INSERT INTO calendario.tipo_notificaciones(
             id_tipo_notificacion, descripcion, icono, id_permisos, id_usuarios_bloqueados)
-    VALUES (108, 'Reposición de cara próxima', 'sigipro-mouse-1', ARRAY[201,1]::INT[], ARRAY[]::INT[]);
+    VALUES (108, 'Reposición de cara próxima', 'sigipro-mouse-1', ARRAY[201]::INT[], ARRAY[]::INT[]);
 
 --Funciones
 CREATE OR REPLACE FUNCTION bioterio.notificacion_retiro()
