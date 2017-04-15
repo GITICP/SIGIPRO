57c0d8976915a6f88e803ee890e6414c90d35af3
Boga <Boga@DESKTOP-V7BSL5V>
2017-04-15 16:21:19 -0600

    Movimiento de inóculos en el menú

---
+++ Scripts Base Datos/Scripts/Z1 - Movimiento del Menú de Inóculo.sql
@@ -0,0 +1,19 @@
+UPDATE seguridad.entradas_menu_principal
+SET id_padre = 400
+WHERE id_menu_principal = 604;
+
+UPDATE seguridad.entradas_menu_principal
+SET tag = 'Inóculos'
+WHERE id_menu_principal = 604;
+
+UPDATE seguridad.entradas_menu_principal
+SET tag = 'Producción'
+WHERE id_menu_principal = 600;
+
+UPDATE seguridad.entradas_menu_principal
+SET tag = 'Catálogo de Producto T.'
+WHERE id_menu_principal = 606;
+
+UPDATE seguridad.entradas_menu_principal
+SET tag = 'Fórmula Maestra'
+WHERE id_menu_principal = 620;
