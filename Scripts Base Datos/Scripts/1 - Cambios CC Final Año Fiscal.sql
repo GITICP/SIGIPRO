ef9d0e6af57682f92c3fdb5da1992520cb0131a6
jdcb9 <jdcb9@DESKTOP-CTRGQD0>
2017-09-25 18:36:49 -0600

    Terminado CRUD de tipos de patrones y controles

--- Scripts Base Datos/Scripts/1 - Cambios CC Final Año Fiscal.sql
+++ Scripts Base Datos/Scripts/1 - Cambios CC Final Año Fiscal.sql
@@ -6,10 +6,74 @@
  * 
  */
 
+-- Agregado el campo de fecha de reporte de los análisis en un informe parcial/final
 ALTER TABLE control_calidad.resultados_analisis_sangrias_prueba ADD fecha_reportado date NULL ;
 ALTER TABLE control_calidad.resultados ADD fecha_reportado date NULL ;
 
+-- Agregada la opción para identificar un análisis como "No realizarR
 ALTER TABLE control_calidad.analisis_grupo_solicitud ADD observaciones_no_realizar varchar(500) NOT NULL DEFAULT 'Sin observaciones.' ;
 ALTER TABLE control_calidad.analisis_grupo_solicitud ADD realizar bool NOT NULL DEFAULT true ;
 
+-- Tabla para la implementación de tipos de patrones y controles
+CREATE TABLE control_calidad.tipos_patronescontroles (
+	id_tipo_patroncontrol serial NOT NULL,
+	nombre varchar(50) NOT NULL,
+	descripcion varchar(500) NOT NULL DEFAULT 'Sin descripción.',
+	tipo varchar(10) NOT NULL,
+	CONSTRAINT tipos_patronescontroles_pk PRIMARY KEY (id_patroncontrol)
+)
+WITH (
+	OIDS=FALSE
+) ;
+
+-- Inserción de tipos de controles y patrones por defecto para los patrones y controles ya existentes
+INSERT INTO control_calidad.tipos_patronescontroles
+(id_tipo_patroncontrol, nombre, descripcion, tipo)
+VALUES(nextval('control_calidad.tipos_patronescontroles_id_tipo_patroncontrol_seq'::regclass), 'Primario', 'Sin descripción.'::character varying, 'Patrón');
+INSERT INTO control_calidad.tipos_patronescontroles
+(id_tipo_patroncontrol, nombre, descripcion, tipo)
+VALUES(nextval('control_calidad.tipos_patronescontroles_id_tipo_patroncontrol_seq'::regclass), 'Secundario', 'Sin descripción.'::character varying, 'Patrón');
+INSERT INTO control_calidad.tipos_patronescontroles
+(id_tipo_patroncontrol, nombre, descripcion, tipo)
+VALUES(nextval('control_calidad.tipos_patronescontroles_id_tipo_patroncontrol_seq'::regclass), 'Control Interno', 'Sin descripción.'::character varying, 'Control');
+
+-- Agregada la referencia a tipos de patrones y controles
+ALTER TABLE control_calidad.patrones ADD id_tipo_patroncontrol int4 NULL ;
+ALTER TABLE control_calidad.patrones ADD CONSTRAINT patrones_tipos_patronescontroles_fk FOREIGN KEY (id_tipo_patroncontrol) REFERENCES control_calidad.tipos_patronescontroles(id_tipo_patroncontrol) ;
+
+-- Update para actualizar las referencias a los patrones y controles ya existentes.
+update control_calidad.patrones
+set id_tipo_patroncontrol = case 
+                                when tipo = 'Primario' then 1 
+                                when tipo = 'Secundario' then 2 
+                                when tipo = 'Control Interno' then 3 
+                                else null
+                            end;
+
+ALTER TABLE control_calidad.patrones ALTER COLUMN id_tipo_patroncontrol SET NOT NULL ;
+
+-- Inserts menu y seguridad
+INSERT INTO seguridad.permisos
+(id_permiso, nombre, descripcion, id_seccion)
+VALUES(580, 'AgregarTipoPatrónControl', 'Permite agregar un tipo de patrón o control.', 2);
+INSERT INTO seguridad.permisos
+(id_permiso, nombre, descripcion, id_seccion)
+VALUES(581, 'EditarTipoPatrónControl', 'Permite editar un tipo de patrón o control.', 2);
+INSERT INTO seguridad.permisos
+(id_permiso, nombre, descripcion, id_seccion)
+VALUES(582, 'EliminarTipoPatrónControl', 'Permite eliminar un tipo de patrón o control.', 2);
+INSERT INTO seguridad.permisos
+(id_permiso, nombre, descripcion, id_seccion)
+VALUES(583, 'VerTipoPatrónControl', 'Permite ver un tipo de patrón o control.', 2);
+
+INSERT INTO seguridad.entradas_menu_principal
+(id_menu_principal, id_padre, tag, redirect, orden)
+VALUES(571, 570, 'Patrones y Controles', '/ControlCalidad/Patron', 1);
+INSERT INTO seguridad.entradas_menu_principal
+(id_menu_principal, id_padre, tag, redirect, orden)
+VALUES(572, 570, 'Tipos de Patrones y Controles', '/ControlCalidad/TipoPatronControl', 2);
+
+UPDATE seguridad.entradas_menu_principal
+SET id_padre=500, tag='Patrones y Controles', redirect=NULL, orden=4
+WHERE id_menu_principal=570;
 
---
+++ web/ControlCalidad/TipoPatronControl/Ver.jsp
@@ -0,0 +1,66 @@
+<%-- 
+    Document   : Ver
+    Created on : Jun 29, 2015, 4:48:27 PM
+    Author     : ld.conejo
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+
+<t:plantilla_general title="Control de Calidad" direccion_contexto="/SIGIPRO">
+
+    <jsp:attribute name="contenido">
+
+        <jsp:include page="../../plantillas/barraFuncionalidad.jsp" />
+
+        <!-- content-wrapper -->
+        <div class="col-md-12 content-wrapper">
+            <div class="row">
+                <div class="col-md-12 ">
+                    <ul class="breadcrumb">
+                        <li>Control de Calidad</li>
+                        <li> 
+                            <a href="/SIGIPRO/ControlCalidad/TipoPatronControl?">Tipos de Patrones y Controles</a>
+                        </li>
+                        <li class="active"> ${tipopatroncontrol.getNombre()} </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- main -->
+            <div class="content">
+                <div class="main-content">
+                    <!-- COLUMN FILTER DATA TABLE -->
+                    <div class="widget widget-table">
+                        <div class="widget-header">
+                            <h3><i class="fa fa-gears"></i> ${tipopatroncontrol.getNombre()} </h3>
+                            <div class="btn-group widget-header-toolbar">
+                                <c:if test="${helper_permisos.validarPermiso(sessionScope.listaPermisos, 582)}">
+                                    <a class="btn btn-danger btn-sm boton-accion confirmable" data-texto-confirmacion="eliminar el tipo de patrón o control" data-href="/SIGIPRO/ControlCalidad/TipoPatronControl?accion=eliminar&id_tipo_patroncontrol=${tipopatroncontrol.getId_tipo_patroncontrol()}">Eliminar</a>
+                                </c:if>
+                                <c:if test="${helper_permisos.validarPermiso(sessionScope.listaPermisos, 581)}">
+                                    <a class="btn btn-warning btn-sm boton-accion" href="/SIGIPRO/ControlCalidad/TipoPatronControl?accion=editar&id_tipo_patroncontrol=${tipopatroncontrol.getId_tipo_patroncontrol()}">Editar</a>
+                                </c:if>
+                            </div>
+                        </div>
+                        ${mensaje}
+                        <div class="widget-content">
+                            <table class="tabla-ver">
+                                <tr><td> <strong>Nombre del Tipo: </strong></td> <td>${tipopatroncontrol.getNombre()} </td></tr>
+                                <tr><td> <strong>Tipo: </strong> <td>${tipopatroncontrol.getTipo()} </td></tr>
+                                <tr><td> <strong>Descripción: </strong> <td>${tipopatroncontrol.getDescripcion()} </td></tr>
+                            </table>
+                            <br>
+                        </div>
+                    </div>
+                    <!-- END WIDGET TICKET TABLE -->
+                </div>
+                <!-- /main-content -->
+            </div>
+            <!-- /main -->
+        </div>
+
+    </jsp:attribute>
+
+</t:plantilla_general>
+
